a947eff8cd081050f368fd2416dba9ba30bc13f6
patrifdz <fdzpatri@gmail.com>
2025-05-18 14:42:44 +0200

    fix: name of interface in model.ts

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,5 @@
-// Variables que se van a usar:
-interface MessageOnStanding {
+// Interface creada para el objeto typeMessage
+interface TypeMessage {
     messageFor4: string;
     messageFor5: string;
     messageFor6and7: string;
@@ -13,7 +13,7 @@ export let userPoints : number;
 // Función para actualizar puntos del jugador (userPoints). Tengo que traer esta función al fichero "model.ts" ya que necesito que el import acceda a la variable a través de una función "setter" puesto que los imports no pueden acceder a valores inmutables, como sería si accede directamente a la variable "userPoints":
 export const updateUserPoints = (newPoints : number) => newPoints >0 ? userPoints = newPoints : userPoints=0;
 
-export const typeMessage : MessageOnStanding = {
+export const typeMessage : TypeMessage = {
     messageFor4 : "Has sido muy conservador",
     messageFor5 : "Te ha entrado el canguelo eh?",
     messageFor6and7 : "Casi casi...",
